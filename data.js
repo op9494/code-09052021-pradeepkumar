97b2475ff10a56933dce9d84c21aa13ec9a5cc80
op9494 <o.pradeep64@gmail.com>
2021-05-09 18:57:23 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,58 @@
+var http = require('http');
+var data=[{"Gender": "Male", "HeightCm": 171, "WeightKg": 96 }, { "Gender": "Male", "HeightCm": 161, "WeightKg":
+  85 }, { "Gender": "Male", "HeightCm": 180, "WeightKg": 77 }, { "Gender": "Female", "HeightCm": 166,
+  "WeightKg": 62}, {"Gender": "Female", "HeightCm": 150, "WeightKg": 70}, {"Gender": "Female",
+  "HeightCm": 167, "WeightKg": 82}];
+  console.log("Before Calculated");
+  console.table(data);
+  var len_data=data.length
+  for(i=0;i<len_data;i++){
+    heightM=data[i].HeightCm/100.0
+    bmi=(data[i].WeightKg/(heightM*heightM));
+    if(bmi<18.5)
+    {
+      BMICategory= "Under Weight";
+      HealthRisk = "Malnutrition risk";
+    }
+    else if(bmi>=18.5&&bmi<=24.9)
+    {
+      BMICategory= "Normal weight";
+      HealthRisk = "Malnutrition risk";
+    }
+    else if (bmi>=25 &&  bmi<=29.9)
+    {
+      BMICategory= "Overweight";
+      HealthRisk = "Low risk";
+    }
+    else if (bmi>=30 &&  bmi<=39.9)
+    {
+      BMICategory= "Moderately obese";
+      HealthRisk = "Enhanced risk";
+    }
+    else if (bmi>=40)
+    {
+      BMICategory= "Severely obese";
+      HealthRisk = "High risk";
+    }
+    else
+    {
+      BMICategory= "Very severely obese";
+      HealthRisk = "Very high risk";
+    }
+    data[i]["BMI"]=bmi;
+    data[i]["BMICategory"]=BMICategory;
+    data[i]["HealthRisk"]=HealthRisk;
+  }
+  console.log("After Calculating BMI");
+  console.table(data);
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type':'text/html'});
+  res.write(
+    'Person        BMICategory     HealthRisk \n');
+  for(i=0;i<len_data;i++){
+    res.write(
+      'Person'+i+"   "+data[i].BMICategory+"   "+data[i].HealthRisk+"\n");
+  }
+  res.end()
+}).listen(8080);
